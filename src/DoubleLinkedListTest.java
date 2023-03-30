9a7a5639a7d9c8d8cf5e819adce008b2d829a41e
Vinayak Jayaprakash <vinayak.jayaprakash02@gmail.com>
2023-03-30 18:45:49 -0400

    Wrote Unit test cases for the insert method

--- src/DoubleLinkedListTest.java
+++ src/DoubleLinkedListTest.java
@@ -25,6 +25,20 @@ class DoubleLinkedListTest {
         assertEquals(dll.head.album.albumTitle, "Test Title 1");
         assertEquals(dll.tail.album.albumTitle, "Test Title 2");
     }
+    @Test
+    public void insertDLL(){
+        DoubleLinkedList dll = new DoubleLinkedList();
+        Album album1 = new Album(1,"Test Title 1",5);
+        dll.insert(0,album1);
+        Album album2 = new Album(2,"Test Title 2",5);
+        dll.insert(1,album2);
+        Album album3 = new Album(3,"Test Title 3",5);
+        dll.insert(2,album3);
+        Album album4 = new Album(4,"Test Title 4",5);
+        dll.insert(3,album4);
+        assertEquals("Test Title 4",dll.tail.album.albumTitle);
+    }
+
 
 
 }
