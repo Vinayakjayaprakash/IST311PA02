f6850657b61f338a831d4e4460cccd3bfdd3d780
Vinayak Jayaprakash <vinayak.jayaprakash02@gmail.com>
2023-03-30 18:34:36 -0400

    Initialized the appendDLL unit test to test the append class

--- src/DoubleLinkedListTest.java
+++ src/DoubleLinkedListTest.java
@@ -13,6 +13,17 @@ class DoubleLinkedListTest {
         assertNull(dll.tail);
 
     }
-
+    @Test
+    public void appendDLL() {
+        DoubleLinkedList dll = new DoubleLinkedList();
+        Album album1 = new Album(1, "Test Title 1", 5);
+        Album album2 = new Album(2, "Test Title 2", 5);
+        dll.append(album1);
+        assertEquals(dll.head.album.albumTitle, "Test Title 1");
+        assertEquals(dll.tail.album.albumTitle, "Test Title 1");
+        dll.append(album2);
+        assertEquals(dll.head.album.albumTitle, "Test Title 1");
+        assertEquals(dll.tail.album.albumTitle, "Test Title 2");
+    }
 
 }
