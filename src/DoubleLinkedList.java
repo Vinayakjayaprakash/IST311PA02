d6119c009104892b4fc8f38b93a586c0bca9fc78
Vinayak Jayaprakash <vinayak.jayaprakash02@gmail.com>
2023-03-30 18:32:43 -0400

    Initalized the append method taken from SLL

--- src/DoubleLinkedList.java
+++ src/DoubleLinkedList.java
@@ -8,4 +8,16 @@ public class DoubleLinkedList {
 
 
     }
+    public Node<Album> append(Album album1) {
+        Node<Album> toAppend = new Node<>(album1);
+        if (this.head == null) {
+            this.head = toAppend; // appends to the head first
+            this.tail = toAppend; // appends to the head first
+        } else { // else it gets added to the next node
+            tail.next = toAppend;
+            toAppend.previous = tail;
+            tail = toAppend;
+        }
+        return toAppend;
+    }
 }
