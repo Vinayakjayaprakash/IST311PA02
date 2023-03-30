10d22c8a44d0bfdfdd3dcd6be005dee7626358dd
Vinayak Jayaprakash <vinayak.jayaprakash02@gmail.com>
2023-03-30 18:27:22 -0400

    Node class has been initialized

--- src/Node.java
+++ src/Node.java
@@ -1,2 +1,13 @@
-public class Node {
+public class Node <Album>{
+    Album album; //Means that the data can be of any type
+    Node <Album> next; //Acts as a point to the next node
+    Node <Album> previous; //Acts as a point to the previous node
+
+    public Node(Album album){
+        this.album = album;
+        this.next = null;
+        this.previous = null;
+
+
+    }
 }
