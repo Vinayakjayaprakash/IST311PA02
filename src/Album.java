60c4473954ad658139a211417685d9be455140fa
Vinayak Jayaprakash <vinayak.jayaprakash02@gmail.com>
2023-03-30 18:30:09 -0400

    Album has been defined and setters and getters have been defined

--- src/Album.java
+++ src/Album.java
@@ -1,2 +1,16 @@
-public class Album {
+import java.util.ArrayList;
+
+public class Album implements Comparable<Album> {
+
+    int albumID;
+    ArrayList<String> artistNames;
+    String albumTitle;
+    int numberOfSongs;
+
+    public Album(int albumID, String title, int numSongs) {
+        this.albumID = albumID;
+        this.albumTitle = title;
+        this.artistNames = new ArrayList<String>();
+        this.numberOfSongs = numSongs;
+    }
 }
