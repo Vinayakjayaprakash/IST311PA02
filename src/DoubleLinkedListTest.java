c15e8184a6541a0495d89ada638a38a458114d1b
Vinayak Jayaprakash <vinayak.jayaprakash02@gmail.com>
2023-03-30 18:34:04 -0400

    Wrote the main unit test class and wrote a unit test to test for the main DLL creating

--- src/DoubleLinkedListTest.java
+++ src/DoubleLinkedListTest.java
@@ -1,5 +1,18 @@
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DoubleLinkedListTest {
+    @Test
+    public void dllTest() {
+
+
+        DoubleLinkedList dll = new DoubleLinkedList();
+        assertNull(dll.head);
+        assertNull(dll.tail);
+
+    }
+
 
 }
